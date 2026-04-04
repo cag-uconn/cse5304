28a5e3300e0730c0bbe807ce69cb1becb1007c45
Omer Khan <khan@uconn.edu>
2026-04-04 23:50:43 +0000

    .

---
+++ project/run_3-tma-memcpy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cuFILE=3-tma-memcpy
+
+nvcc -lcuda -O3 --use_fast_math -gencode arch=compute_90a,code=[sm_90a,compute_90a] --expt-relaxed-constexpr --std=c++20 $cuFILE.cu -o $cuFILE.out
+
+./$cuFILE.out
